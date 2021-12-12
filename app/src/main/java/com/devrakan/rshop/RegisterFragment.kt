3e9b4a419a5fbcd651bea748440ec06a0436c1e5
irakan4 <rakan.naif.jed.kt2@tuwaiq.edu.sa>
2021-12-13 00:00:13 +0300

    added registerFragment&LoginFragment + save user info to firebase

--- app/src/main/java/com/devrakan/rshop/RegisterFragment.kt
+++ app/src/main/java/com/devrakan/rshop/RegisterFragment.kt
@@ -1,28 +1,26 @@
 package com.devrakan.rshop
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_register.view.*
+import kotlin.concurrent.timerTask
 
 
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -30,26 +28,92 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+        view.btn_register.setOnClickListener {
+            val userName = view.usernameSingup.text.toString()
+            val email = view.email_singup.text.toString()
+            val password = view.password_singup.text.toString()
+            val phone = view.phone_singup.text.toString()
+
+            when {
+                TextUtils.isEmpty(phone) ->
+                    Toast.makeText(
+                        activity?.applicationContext,
+                        "phone number is required",
+                        Toast.LENGTH_LONG
+                    ).show()
+                TextUtils.isEmpty(userName) ->
+                    Toast.makeText(
+                        activity?.applicationContext,
+                        "user name is required",
+                        Toast.LENGTH_LONG
+                    ).show()
+                TextUtils.isEmpty(password) ->
+                    Toast.makeText(
+                        activity?.applicationContext,
+                        "password is required",
+                        Toast.LENGTH_LONG
+                    ).show()
+                TextUtils.isEmpty(email) ->
+                    Toast.makeText(
+                        activity?.applicationContext,
+                        "Email is required",
+                        Toast.LENGTH_LONG
+                    ).show()
+                else -> {
+                    var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val currentUserId =
+                                    FirebaseAuth.getInstance().currentUser?.uid.toString()
+                                val usersRef: DatabaseReference =
+                                    FirebaseDatabase.getInstance().reference.child("Users")
+                                var userMap = HashMap<String, Any>()
+                                userMap["uid"] = currentUserId
+                                userMap["username"] = userName.toLowerCase()
+                                userMap["phone"] = phone.toLowerCase()
+                                userMap["email"] = email.toLowerCase()
+                                usersRef.child(currentUserId).setValue(userMap)
+                                    .addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+
+                                            Toast.makeText(
+                                                activity?.applicationContext,
+                                                "account has been created successfully",
+                                                Toast.LENGTH_LONG
+                                            )
+                                                .show()
+                                            val intent =
+                                                Intent(
+                                                    activity?.applicationContext,
+                                                    MainActivity::class.java
+                                                )
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                                            startActivity(intent)
+
+                                        } else {
+                                            val msg = task.exception!!.toString()
+                                            Toast.makeText(
+                                                activity?.applicationContext,
+                                                "Error: $msg",
+                                                Toast.LENGTH_LONG
+                                            )
+                                            mAuth.signOut()
+
+
+                                        }
+                                    }
+
+
+                            }
+
+                        }
+                }
+
+            }
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
