f674946e572b54bd24b6316896ae20cc35426628
irakan4 <rakan.naif.jed.kt2@tuwaiq.edu.sa>
2021-12-12 23:16:02 +0300

    added LoginActivity

--- app/src/main/java/com/devrakan/rshop/LoginActivity.kt
+++ app/src/main/java/com/devrakan/rshop/LoginActivity.kt
@@ -3,7 +3,10 @@ package com.devrakan.rshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,14 +14,39 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         var login = LoginFragment()
         var register = RegisterFragment()
+
+        // change the color of btn login //
+        btn_login_login.setBackgroundColor(ContextCompat.getColor(this,R.color.purple_200))
+
+        // make the login fragment the first fragment //
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container,login,"Login")
             .add(R.id.fragment_container,register,"Register")
             .show(login)
             .hide(register)
             .commit()
+        // btn login . On Click , && show login fragment, hide register fragment, change the btn color & toolbar title//
+        // and also change the color of btn registerColor
         btn_login_login.setOnClickListener {
-            supportFragmentManager
+            supportFragmentManager.beginTransaction()
+                .show(login)
+                .hide(register)
+                .commit()
+            toolbar.text = "Login"
+            btn_login_login.setBackgroundColor(ContextCompat.getColor(this,R.color.purple_200))
+            btn_login_register.setBackgroundColor(ContextCompat.getColor(this,R.color.teal_700))
+        }
+        // btn register . On Click , && show register fragment, hide login fragment, change the btn color & toolbar title//
+        // and also change the color of btn registerColor
+
+        btn_login_register.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .show(register)
+                .hide(login)
+                .commit()
+            btn_login_login.setBackgroundColor(ContextCompat.getColor(this,R.color.teal_700))
+            btn_login_register.setBackgroundColor(ContextCompat.getColor(this,R.color.purple_200))
+
         }
     }
 }
--- app/src/main/java/com/devrakan/rshop/LoginFragment.kt
+++ app/src/main/java/com/devrakan/rshop/LoginFragment.kt
@@ -5,20 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    var firebaseAuth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -26,26 +21,11 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        return view
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
